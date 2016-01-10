0f9418eed089938e0094f40cc15682ef59e041a1
alexandermendes <alexanderhmendes@gmail.com>
2016-01-10 06:17:19 +0000

    Add URL rule to set Gravatar for current user

--- __init__.py
+++ __init__.py
@@ -3,9 +3,11 @@
 import default_settings
 from flask.ext.plugins import Plugin
 from flask import current_app as app
+from flask import redirect
 from pybossa_gravatar.gravatar import Gravatar
 from pybossa.model.user import User
 from sqlalchemy import event
+from flask.ext.login import current_user
 
 __plugin__ = "PyBossaGravatar"
 __version__ = "0.1.0"
@@ -22,6 +24,7 @@ class PyBossaGravatar(Plugin):
         self.load_config()
         gravatar.init_app(app)
         self.setup_event_listener()
+        self.setup_url_rule()
     
     
     def load_config(self):
@@ -40,4 +43,17 @@ class PyBossaGravatar(Plugin):
         def add_user_event(mapper, conn, target):
             """Set gravatar by default for new users."""
             gravatar.set(target, update_repo=False)
+    
+    
+    def setup_url_rule(self):
+        """Setup URL rule."""
+        
+        @app.route('/account/set-gravatar')
+        def set_gravatar(self):
+            """Set gravatar for the current user."""
+            if current_user.is_anonymous():
+                return redirect(url_for('account.signin'))
+            
+            gravatar.set(current_user)
+            
             
